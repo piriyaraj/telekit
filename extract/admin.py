d3a678e0b3f0df150b89833130412e127f85e34c
piriyaraj <piriyaraj1998@gmail.com>
2022-10-24 22:42:49 +0530

    fixed notification panel error

--- extract/admin.py
+++ extract/admin.py
@@ -11,8 +11,11 @@ from django.utils.html import format_html
 class NotiAdmin(admin.ModelAdmin):
     list_display = ("name",'date',"show_average","viewed")
     list_editable=['viewed']
+    ordering = ("viewed", "date")
+    list_filter = ["viewed"]
     def show_average(self, obj):
         result=Link.objects.get(id=obj.link.id)
-        return format_html("<a href='/join/{}' target='_blank'>view</a></br>category: {}<br/>Name: {}</br>Description: {}", result.linkId,result.category,result.name)
+        return format_html("<a href='/join/{}' target='_blank'>view</a></br>category: {}<br/>Name: {}</br>Description: {}", result.linkId,result.category,result.name,result.description)
 
+    
     show_average.short_description = "Link"
