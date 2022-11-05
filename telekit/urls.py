7ccc2af1af942dfacd76b5f96b695c4d3f1a3394
piriyaraj <piriyaraj1998@gmail.com>
2022-11-05 19:16:23 +0530

    added ads txt

--- telekit/urls.py
+++ telekit/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', include('blog.urls')),
@@ -24,5 +25,7 @@ urlpatterns = [
     path('', include('user_profile.urls')),
     path('superuser/', include('extract.urls')),
     path('robots.txt', include('robots.urls')),
+    path('ads.txt', TemplateView.as_view(template_name='text/ads.txt', content_type='text/plain')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
