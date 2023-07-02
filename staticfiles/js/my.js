a9bc124fabc32a5f947073881515db9a7d916546
piriyaraj <piriyaraj1998@gmail.com>
2023-07-02 14:33:02 +0530

    updated

--- staticfiles/js/my.js
+++ staticfiles/js/my.js
@@ -1,6 +1,22 @@
-
 function imgError(image) {
-    image.onerror = "";
-    image.src = "/static/images/nopic.jpg";
-    return true;
+  image.onerror = "";
+  image.src = "/static/images/nopic.jpg";
+  return true;
 }
+
+(async () => {
+  "use strict";
+
+  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+  const $ = (x) => document.querySelector(x);
+
+  await delay(100);
+  const elem = $(".dialog-box");
+
+  if (elem) {
+    elem.style.visibility = "hidden";
+    await delay(1000 + 4000 * Math.random());
+    console.log(new Date().toLocaleTimeString() + ": Clicked");
+    $(".here-button").click();
+  }
+})();
