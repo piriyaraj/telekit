50362af7a5cce1545d25b22100bbf5891a8f8a86
piriyaraj <piriyaraj1998@gmail.com>
2024-01-25 14:43:25 +0530

    Fixed: revalidate cache

--- telekit/urls.py
+++ telekit/urls.py
@@ -1,22 +1,9 @@
-"""telekit URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+from django.views.decorators.cache import cache_control
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
@@ -28,5 +15,7 @@ urlpatterns = [
     path('robots.txt', include('robots.urls')),
     path('ads.txt', TemplateView.as_view(template_name='text/ads.txt', content_type='text/plain')),
 
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    # Serve static files with cache control
+    path('static/<path:path>', cache_control(max_age=31536000, must_revalidate=True)(serve), {'document_root': settings.STATICFILES_DIRS[0]}),
+    # max_age is set to 31536000 seconds (1 year)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
