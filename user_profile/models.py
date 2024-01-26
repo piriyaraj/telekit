cd88d8e859ab2cafe045ecabaaf8aa321ba6cb53
piriyaraj <piriyaraj1998@gmail.com>
2024-01-26 22:53:06 +0530

    Changed: Default value as 0

--- user_profile/models.py
+++ user_profile/models.py
@@ -16,7 +16,7 @@ class User(AbstractUser):
     verified = models.BooleanField(default=False)
     added=models.DateTimeField(auto_now_add=True)
     modified=models.DateTimeField(auto_now=True)
-    points = models.IntegerField(default = 100)
+    points = models.IntegerField(default = 0)
     verification_id = models.CharField(max_length=30,default='aaa')
     REQUIRED_FIELDS = ["email"]
     objects = CustomUserManager()
