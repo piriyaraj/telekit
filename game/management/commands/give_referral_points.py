8ca1f858ee8aca241539637bbddea520a1087d1e
piriyaraj <piriyaraj1998@gmail.com>
2024-03-25 20:24:00 +0530

    Updated: referral mail for participation

--- game/management/commands/give_referral_points.py
+++ game/management/commands/give_referral_points.py
@@ -1,12 +1,12 @@
 # your_app/management/commands/update_points_per_day.py
+import time
 from django.core.management.base import BaseCommand
 from game.models import ReferralCount,ReferralVisit
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-def send_email(body, to_email):
-    subject = "Congratulations! You're Today's Referral Game Champion! Telekit.link"
+def send_email(body, to_email,subject):
 
     sender_email = "support@telekit.link"
     password = "1998Piriyaraj@"  # Use the email account’s password
@@ -48,18 +48,50 @@ We're thrilled to announce that you've won by attracting the most visitors to yo
 
 This achievement demonstrates your dedication and ability to engage others, and we're grateful for your contribution to our community.
 
-Keep up the excellent work, and continue spreading the word about our platform!
+Keep up the excellent work, and continue spreading the word about our platform by sharing your referral link: [referral_link]
 
 Best regards,
 Telekit.link
 """
-        referral = ReferralCount.objects.all().order_by("-count")
-        if referral:
-            referral = referral[0]
+        body_not_win ="""
+Dear [Username],
+
+We want to extend our sincere appreciation for your participation in our Referral Game!
+
+While you may not have won today's competition, your efforts and engagement have not gone unnoticed. Your dedication to spreading the word about our platform is invaluable to us.
+
+As a token of our gratitude, we've credited your account with 100 points for your participation.
+
+Thank you for being an active member of our community, and we encourage you to continue sharing your referral link: [referral_link]. Every effort counts towards building a stronger community!
+
+Best regards,
+Telekit.link
+        """
+        referral_list = ReferralCount.objects.all().order_by("-count")
+        if referral_list:
+            referral = referral_list[0]
             referral.user.points = referral.user.points + 1000
             referral.user.save()
             body = body.replace("[Username]",referral.user.username)
-            send_email(body,referral.user.email)
-        ReferralCount.objects.all().delete()
+            body = body.replace("[referral_link]",f"https://telekit.link/ref/telekit{referral.user.id}")
+            subject = "Congratulations! You're Today's Referral Game Champion! Telekit.link"
+            try:
+                send_email(body, referral.user.email, subject)
+            except:
+                pass
+            referral.delete()
+        for ref in referral_list[1:]: 
+            referral = ref
+            referral.user.save()
+            referral.user.points = referral.user.points + 100
+            body_not_win = body_not_win.replace("[Username]",referral.user.username)
+            body_not_win = body_not_win.replace("[referral_link]",f"https://telekit.link/ref/telekit{referral.user.id}")
+            subject = "Thank You for Participating in Our Referral Game!"
+            try:
+                send_email(body_not_win,referral.user.email, subject)
+            except:
+                pass
+            referral.delete()
+            time.sleep(5)
         ReferralVisit.objects.all().delete()
         self.stdout.write(self.style.SUCCESS('Successfully updated points per day'))
